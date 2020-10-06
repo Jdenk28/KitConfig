cd17a165e6e411039dcbc22e2f4ca0280815d4f9
Jdenk28 <33169744+Jdenk28@users.noreply.github.com>
2020-10-06 19:31:03 -0400

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,167 @@
+/*
+$(document).ready(function() {
+    $("#cabinet-icon").click(function() {
+        $("#cabinet-dropdown").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#island-icon").click(function() {
+        $("#island-dropdown").toggle();
+    });
+});
+
+$(document).ready(function() {
+    $("#backsplash-icon").click(function() {
+        $("#backsplash-dropdown").toggle();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#countertop-icon").click(function() {
+        $("#countertop-dropdown").toggle();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#floor-icon").click(function() {
+        $("#floor-dropdown").toggle();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#hood-icon").click(function() {
+        $("#hood-dropdown").toggle();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#oven-icon").click(function() {
+        $("#oven-dropdown").toggle();
+    });
+});
+
+
+$(document).ready(function() {
+    $("#fridge-icon").click(function() {
+        $("#fridge-dropdown").toggle();
+    });
+});
+*/
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+//CABINET
+
+$('#cabinet_1').click(function() {
+  $('#cabinet_img').attr('src', 'resources/img/cabinet_1.png');
+});
+
+$('#cabinet_2').click(function() {
+  $('#cabinet_img').attr('src', 'resources/img/cabinet_2.png');
+});
+
+$('#cabinet_3').click(function() {
+  $('#cabinet_img').attr('src', 'resources/img/cabinet_3.png');
+});
+
+//ISLAND
+
+$('#island_1').click(function() {
+  $('#island_img').attr('src', 'resources/img/island_1.png');
+});
+
+$('#island_2').click(function() {
+  $('#island_img').attr('src', 'resources/img/island_2.png');
+});
+
+$('#island_3').click(function() {
+  $('#island_img').attr('src', 'resources/img/island_3.png');
+});
+
+//FLOORING
+
+$('#floor_1').click(function() {
+  $('#floor_img').attr('src', 'resources/img/floor_1.png');
+});
+
+$('#floor_2').click(function() {
+  $('#floor_img').attr('src', 'resources/img/floor_2.png');
+});
+
+$('#floor_3').click(function() {
+  $('#floor_img').attr('src', 'resources/img/floor_3.png');
+});
+
+//BACKSPLASH
+
+$('#backsplash_1').click(function() {
+  $('#backsplash_img').attr('src', 'resources/img/backsplash_1.png');
+});
+
+$('#backsplash_2').click(function() {
+  $('#backsplash_img').attr('src', 'resources/img/backsplash_2.png');
+});
+
+$('#backsplash_3').click(function() {
+  $('#backsplash_img').attr('src', 'resources/img/backsplash_3.png');
+});
+
+//COUNTER TOP
+
+$('#countertop_1').click(function() {
+  $('#countertop_img').attr('src', 'resources/img/countertop_1.png');
+});
+
+$('#countertop_2').click(function() {
+  $('#countertop_img').attr('src', 'resources/img/countertop_2.png');
+});
+
+$('#countertop_3').click(function() {
+  $('#countertop_img').attr('src', 'resources/img/countertop_3.png');
+});
+
+//OVEN
+
+$('#oven_1').click(function() {
+  $('#oven_img').attr('src', 'resources/img/oven_1.png');
+});
+
+$('#oven_2').click(function() {
+  $('#oven_img').attr('src', 'resources/img/oven_2.png');
+});
+
+$('#oven_3').click(function() {
+  $('#oven_img').attr('src', 'resources/img/oven_3.png');
+});
+
+//REFRIGERATOR
+
+$('#fridge_1').click(function() {
+  $('#fridge_img').attr('src', 'resources/img/fridge_1.png');
+});
+
+$('#fridge_2').click(function() {
+  $('#fridge_img').attr('src', 'resources/img/fridge_2.png');
+});
+
+//HOOD
+
+$('#hood_1').click(function() {
+  $('#hood_img').attr('src', 'resources/img/hood_1.png');
+});
+
+$('#hood_2').click(function() {
+  $('#hood_img').attr('src', 'resources/img/hood_2.png');
+});
+
+$('#hood_3').click(function() {
+  $('#hood_img').attr('src', 'resources/img/hood_3.png');
+});
